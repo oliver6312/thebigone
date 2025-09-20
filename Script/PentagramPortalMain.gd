8c8db086d33e403aa025bb265972383d0a281cb1
oliver6312 <oliver.juhl.05@gmail.com>
2025-09-20 13:10:22 +0200

    Halfway to portal

--- Script/PentagramPortalMain.gd
+++ Script/PentagramPortalMain.gd
@@ -1,14 +1,45 @@
 extends CharacterBody2D
 
 #@export var target_position: Vector2
-@onready var circle: CollisionShape2D = $Circle
+@onready var flame_1: Sprite2D = $flame_1
+@onready var flame_2: Sprite2D = $flame_2
+@onready var flame_3: Sprite2D = $flame_3
+@onready var flame_4: Sprite2D = $flame_4
+@onready var flame_5: Sprite2D = $flame_5
+@onready var teleport: CharacterBody2D = $teleport
+@onready var circle: CollisionShape2D = $teleport/Circle
 
-func portal():
-	pass
+
+func _ready() -> void:
+	flame_1.visible = false
+	flame_2.visible = false
+	flame_3.visible = false
+	flame_4.visible = false
+	flame_5.visible = false
+	circle.disabled = true
 
 
+func _on_firezone_1_body_entered(body: Node2D) -> void:
+	if body.has_method("fireball"):
+		print("The fire is lit")
+		flame_1.visible = true
 
-#func _on_portal_body_entered(body: Node2D) -> void:
-#	print ("Player in portal")
-#	if body.has_method("player"):
-#		body.global_position = Vector2(-10, 10)
+func _on_firezone_2_body_entered(body: Node2D) -> void:
+	if body.has_method("fireball"):
+		print("The fire is lit")
+		flame_2.visible = true
+
+func _on_firezone_3_body_entered(body: Node2D) -> void:
+	if body.has_method("fireball"):
+		print("The fire is lit")
+		flame_3.visible = true
+
+func _on_firezone_4_body_entered(body: Node2D) -> void:
+	if body.has_method("fireball"):
+		print("The fire is lit")
+		flame_4.visible = true
+
+func _on_firezone_5_body_entered(body: Node2D) -> void:
+	if body.has_method("fireball"):
+		print("The fire is lit")
+		flame_5.visible = true
